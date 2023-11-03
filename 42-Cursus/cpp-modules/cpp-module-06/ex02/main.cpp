ccc50ee1c3c32f798dae88157885399a81909a2f
Omer faruk Kavak <okavak@k1m34s03.42istanbul.com.tr>
2023-11-03 21:38:41 +0300

    cpp06 updated

--- 42-Cursus/cpp-modules/cpp-module-06/ex02/main.cpp
+++ 42-Cursus/cpp-modules/cpp-module-06/ex02/main.cpp
@@ -31,16 +31,32 @@ void identify(Base& p)
 {
     try
     {
-        if (dynamic_cast<A*>(&p))
-            std::cout << "A" << std::endl;
-        else if (dynamic_cast<B*>(&p))
-            std::cout << "B" << std::endl;
-        else if (dynamic_cast<C*>(&p))
-            std::cout << "C" << std::endl;
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+        
     }
-    catch (const std::exception& e)
+    catch (std::exception& e)
     {
-        std::cerr << e.what() << std::endl;
+       
+        try
+        {
+            (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+        }
+        catch(const std::exception& e)
+        {
+            try
+            {
+                (void)dynamic_cast<C&>(p);
+            std::cout << "C" << std::endl;
+            }
+            catch(const std::exception& e)
+            {
+                std::cerr << e.what() << '\n';
+            }
+            
+        }
+        
     }
 }
 
